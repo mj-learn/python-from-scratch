09ff85c1b13dd40e9c6b9d25988b5b00039fbdbf
MOnjofn <monjofn@gmail.com>
2024-07-30 19:10:05 +0300

    add(course): coding exercises of day 24

--- 01-100-days-of-code-python/024-intermediate-files-dirs-paths/02-relative-vs-absolute-paths.py
+++ 01-100-days-of-code-python/024-intermediate-files-dirs-paths/02-relative-vs-absolute-paths.py
@@ -1,2 +1,14 @@
 # Absolute path: Specifies a complete path from the root directory (e.g., /home/user/file.txt).
 # Relative path: Specifies a path relative to the current working directory (e.g., ../file.txt).
+
+# Relative to sub folders in current folder
+with open("./sub_folder/file.txt") as file:
+    content = file.read()
+
+print(content)
+
+# Relative for upper folder from current folder (use ../../ for more up levels)
+with open("../024-intermediate-snake-game-improved/highscore.txt") as file:
+    content = file.read()
+
+print(content)
